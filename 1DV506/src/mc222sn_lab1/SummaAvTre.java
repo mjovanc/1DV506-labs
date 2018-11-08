3f9c76ef33d645b30733778e93016c4096152ace
qexcc <marcus.cvjeticanin@outlook.com>
2018-11-08 23:49:26 +0100

    Adding new class SummaAvTre

---
+++ 1DV506/src/mc222sn_lab1/SummaAvTre.java
@@ -0,0 +1,18 @@
+package mc222sn_lab1;
+
+import java.util.Scanner;
+
+public class SummaAvTre {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Ange ett tresiffrigt heltal: ");
+		int number = in.nextInt();
+		
+		
+		
+		System.out.println("Summan av siffrorna i heltalet är: ");
+	}
+
+}
