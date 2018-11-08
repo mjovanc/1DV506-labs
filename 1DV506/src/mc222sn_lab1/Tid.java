485ee3db8a54cda474d11aa9ef4f5a4ca46e0f0d
qexcc <marcus.cvjeticanin@outlook.com>
2018-11-08 17:42:05 +0100

    Adding code to main method

--- 1DV506/src/mc222sn_lab1/Tid.java
+++ 1DV506/src/mc222sn_lab1/Tid.java
@@ -1,8 +1,17 @@
 package mc222sn_lab1;
 
+import java.util.Scanner;
+
 public class Tid {
 	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.println("Ange antal sekunder: ");
+		int seconds = in.nextInt();
+		
+		
+		
+		System.out.println("Detta motsvarar: ");
 	}
 }
