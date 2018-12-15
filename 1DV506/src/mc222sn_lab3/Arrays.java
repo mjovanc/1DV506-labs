a7b8663a984764f8ccfc626a483bad600302cb55
qexcc <mc222sn@student.lnu.se>
2018-12-15 13:36:18 +0100

    Adding code

--- 1DV506/src/mc222sn_lab3/Arrays.java
+++ 1DV506/src/mc222sn_lab3/Arrays.java
@@ -2,12 +2,15 @@ package mc222sn_lab3;
 
 public class Arrays
 {
-
 	public static void main(String[] args)
 	{
-		int[] n = {3,4,5,6,7};
-	    String str = Arrays.toString(n);
-	    System.out.println("n = " + str);
+		int[] n = {3,4,5,6,7,1,2,5,3,8};
+		int[] newArr = Arrays.sort(n);
+		
+		for (int i : newArr)
+		{
+			System.out.println(i);
+		}
 	}
 	
 	public static int sum(int[] arr)
@@ -45,6 +48,84 @@ public class Arrays
 	
 	public static int[] addN(int[] arr, int n)
 	{
+		int[] newArr = new int[arr.length];
 		
+		for (int i = 0; i < arr.length; i++)
+		{
+			newArr[i] = arr[i] + n;
+		}
+		
+		return newArr;
+	}
+	
+	public static int[] reverse(int[] arr)
+	{
+		int[] newArr = new int[arr.length];
+		int index = 0;
+		
+		for (int i = arr.length - 1; i >= 0; i--)
+		{
+			newArr[index] = arr[i];
+			index++;
+		}
+		
+		return newArr;
+	}
+	
+	public static boolean hasN(int[] arr, int n)
+	{
+		for (int i : arr)
+		{
+			if (i == n) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void replaceAll(int[] arr, int old, int nw)
+	{
+		for (int i : arr)
+		{
+			if (i == old)
+			{
+				i = nw;
+			}
+		}
+	}
+	
+	public static int[] sort(int[] arr)
+	{
+		int[] newArr = new int[arr.length];
+		int temp = 0;
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			newArr[i] = arr[i];
+		}
+		
+		for (int i = 0; i < newArr.length; i++)
+		{
+			for (int j = 0; j < newArr.length; j++)
+			{
+				if (newArr[i] > newArr[j])
+				{
+					temp = newArr[i];
+					
+					newArr[i] = newArr[j];
+					newArr[j] = temp;
+				}
+			}
+		}
+		
+		return newArr;
 	}
 }
+
+
+
+
+
+
+
+
