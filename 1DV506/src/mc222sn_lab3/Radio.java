21ecfa1c28ddc553dfc3517376f516f8bdbc7714
qexcc <mc222sn@student.lnu.se>
2018-12-17 12:19:09 +0100

    Adding code

--- 1DV506/src/mc222sn_lab3/Radio.java
+++ 1DV506/src/mc222sn_lab3/Radio.java
@@ -24,42 +24,123 @@ public class Radio
 	{
 		on = true;
 	}
+	
+	public void turnOff()
+	{
+		on = false;
+	}
 
-	public void setVolume(int vol)
+	public void setVolume(int newVolume)
 	{
 		if (on)
 		{
-			if (vol >= 0 && vol <= 5)
+			if (newVolume >= 0 && newVolume <= 5)
 			{
-				volume = vol;
+				volume = newVolume;
 			}
 			else
 			{
-				System.err.println("New volume not within range!");
+				System.err.println("out of range = ==> error and neglect");
 			}
 		}
 		else
 		{
-			System.err.println("No adjustment possible");
+			System.err.println("Radio off ==> No adjustment possible");
 		}
 	}
 	
-	public void setChannel(int ch)
+	public void setChannel(int newChannel)
 	{
 		if (on)
 		{
-			if (ch >= 1 && ch <= 10)
+			if (newChannel >= 1 && newChannel <= 10)
 			{
-				channel = ch;
+				channel = newChannel;
 			}
 			else
 			{
-				System.err.println("New channel not within range!");
+				System.err.println("out of range = ==> error and neglect");
 			}
 		}
 		else
 		{
-			System.err.println("No adjustment possible");
+			System.err.println("Radio off ==> No adjustment possible");
+		}
+	}
+	
+	public void volumeUp()
+	{
+		if (on)
+		{
+			if (volume < 5)
+			{
+				volume = volume + 1;
+			}
+			else
+			{
+				System.err.println("out of range = ==> error and neglect");
+			}
+		}
+		else
+		{
+			System.err.println("Radio off ==> No adjustment possible");
+		}
+	}
+	
+	public void volumeDown()
+	{
+		if (on)
+		{
+			if (volume > 0)
+			{
+				volume = volume - 1;
+			}
+			else
+			{
+				System.err.println("volume < 0 ==> error and neglect");
+			}
+		}
+		else
+		{
+			System.err.println("Radio off ==> No adjustment possible");
+		}
+	}
+	
+	public void channelUp()
+	{
+		if (on)
+		{
+			if (channel < 10)
+			{
+				channel = channel + 1;
+			}
+			else
+			{
+				System.err.println("No more channels up!");
+			}
+		}
+		else
+		{
+			System.err.println("Radio off ==> No adjustment possible");
+		}
+	}
+	
+	public void channelDown()
+	{
+		if (on)
+		{
+			if (channel > 1)
+			{
+				channel = channel - 1;
+			}
+			else
+			{
+				System.err.println("No more channels down!");
+			}
+		}
+		else
+		{
+			System.err.println("Radio off ==> No adjustment possible");
 		}
 	}
 	
