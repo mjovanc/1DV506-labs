348b47a2ae0ebbd89b22b69e1c52435184adebb0
qexcc <mc222sn@student.lnu.se>
2018-12-18 22:38:11 +0100

    Adding new class

---
+++ 1DV506/src/mc222sn_lab3/Card.java
@@ -0,0 +1,18 @@
+package mc222sn_lab3;
+
+public class Card
+{
+	private int rank;
+	private String suit;
+	
+	public Card(int r, String s)
+	{
+		rank = r;
+		suit = s;
+	}
+	
+	public int getRank()
+	{
+		return rank;
+	}
+}
