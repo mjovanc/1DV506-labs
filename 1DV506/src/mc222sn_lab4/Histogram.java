4eb8e383e87f8f972f21f581950307493358d6d0
qexcc <mc222sn@student.lnu.se>
2019-01-11 16:00:40 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/Histogram.java
+++ 1DV506/src/mc222sn_lab4/Histogram.java
@@ -1,9 +1,33 @@
 package mc222sn_lab4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Histogram
 {
 	public static void main(String[] args)
 	{
+		Histogram.print("/home/qexcc/Documents/heltal.dat");
+	}
+	
+	public static void print(String path)
+	{
+		File inputNumbers = new File(path);
 		
+		try
+		{
+			Scanner in = new Scanner(inputNumbers);
+			
+			while(in.hasNextInt())
+			{
+				int value = in.nextInt();
+				System.out.println(value);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("");
+		}
 	}
 }
