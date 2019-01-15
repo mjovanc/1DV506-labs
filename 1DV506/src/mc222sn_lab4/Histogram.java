b837843305a7aa32e57895cee67d3f8d0ce88806
qexcc <mc222sn@student.lnu.se>
2019-01-15 12:41:36 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/Histogram.java
+++ 1DV506/src/mc222sn_lab4/Histogram.java
@@ -8,26 +8,107 @@ public class Histogram
 {
 	public static void main(String[] args)
 	{
-		Histogram.print("/home/qexcc/Documents/heltal.dat");
+		String path = "/home/qexcc/Documents/heltal.dat";
+		File inputNumbers = new File(path);
+		
+		System.out.printf("Läser heltal från filen: %s\n", path);
+		System.out.printf("Antal i intervallet [1,100]: %d\n", Histogram.countInInterval(inputNumbers));
+		System.out.printf("Övriga: %d\n", Histogram.countOutInterval(inputNumbers));
+		
+		System.out.println("Histogram");
+		for (int i = 1; i < 100; i = i + 10)
+		{
+			System.out.printf("  %-2d - %-3d | \n", i, i + 9);
+		}
+		
+		System.out.println();
+		Histogram.occurs(inputNumbers);
 	}
 	
-	public static void print(String path)
+	public static int countInInterval(File numbers)
 	{
-		File inputNumbers = new File(path);
+		int count = 0;
 		
 		try
 		{
-			Scanner in = new Scanner(inputNumbers);
-			
+			Scanner in = new Scanner(numbers);
+
 			while(in.hasNextInt())
 			{
 				int value = in.nextInt();
-				System.out.println(value);
+				
+				if (value > 0 && value <= 100)
+				{
+					count++;
+				}
 			}
+			
+			in.close();
 		}
 		catch (FileNotFoundException e)
 		{
-			System.err.println("");
+			System.err.println("Could not find the file.");
+		}
+		
+		return count;
+	}
+	
+	public static int countOutInterval(File numbers)
+	{
+		int count = 0;
+		
+		try
+		{
+			Scanner in = new Scanner(numbers);
+
+			while(in.hasNextInt())
+			{
+				int value = in.nextInt();
+				
+				if (value < 1 || value > 100)
+				{
+					count++;
+				}
+			}
+			
+			in.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("Could not find the file.");
+		}
+		
+		return count;
+	}
+	
+	public static void occurs(File numbers)
+	{
+		int count = 0;
+		
+		try
+		{
+			Scanner in = new Scanner(numbers);
+
+			while(in.hasNextInt())
+			{
+				int value = in.nextInt();
+				
+				if (value > 0 && value <= 10)
+				{
+					count++;
+				}
+			}
+			
+			for (int i = 0; i < count; i++)
+			{
+				System.out.print("*");
+			}
+			
+			in.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("Could not find the file.");
 		}
 	}
 }
