80c87260cbb91cb3bedec464a44d73ca7847fa16
qexcc <mc222sn@student.lnu.se>
2019-01-15 14:21:14 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/RaknaTecken.java
+++ 1DV506/src/mc222sn_lab4/RaknaTecken.java
@@ -10,7 +10,8 @@ public class RaknaTecken
 	{
 		String path = args[0];
 		File inputNumbers = new File(path);
-		System.out.println(RaknaTecken.capitalLettersCount(inputNumbers));
+		System.out.printf("Antal stora bokstäver: %d\n", RaknaTecken.capitalLettersCount(inputNumbers));
+		System.out.printf("Antal små bokstäver: %d\n", RaknaTecken.lowercaseLettersCount(inputNumbers));
 	}
 	
 	private static int capitalLettersCount(File inputFile) throws FileNotFoundException
@@ -27,6 +28,33 @@ public class RaknaTecken
 			{
 				char letter = line.charAt(i);
 				boolean upper = Character.isUpperCase(letter);
+				
+				if (upper)
+				{
+					count++;
+				}
+			}
+		}
+		
+		in.close();
+		
+		return count;
+	}
+	
+	private static int lowercaseLettersCount(File inputFile) throws FileNotFoundException
+	{
+		Scanner in = new Scanner(inputFile);
+		
+		int count = 0;
+		
+		while(in.hasNextLine())
+		{
+			String line = in.nextLine();
+			
+			for (int i = 0; i < line.length(); i++)
+			{
+				char letter = line.charAt(i);
+				boolean upper = Character.isLowerCase(letter);
 				
 				if (upper)
 				{
