fc7fbf2d29c02a68a53fb23257e1122ba125f999
qexcc <mc222sn@student.lnu.se>
2019-01-17 22:50:14 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/RaknaTecken.java
+++ 1DV506/src/mc222sn_lab4/RaknaTecken.java
@@ -13,6 +13,7 @@ public class RaknaTecken
 		System.out.printf("Antal stora bokstäver: %d\n", RaknaTecken.cLettersCount(inputNumbers));
 		System.out.printf("Antal små bokstäver: %d\n", RaknaTecken.lcLettersCount(inputNumbers));
 		System.out.printf("Antal \"whitespaces\": %d\n", RaknaTecken.wsCount(inputNumbers));
+		System.out.printf("Övriga tecken: %d", RaknaTecken.otherChars(inputNumbers));
 	}
 	
 	private static int cLettersCount(File inputFile) throws FileNotFoundException
@@ -85,6 +86,35 @@ public class RaknaTecken
 				boolean whiteSpace = Character.isWhitespace(letter);
 				
 				if (whiteSpace)
+				{
+					count++;
+				}
+			}
+		}
+		
+		in.close();
+		
+		return count;
+	}
+	
+	private static int otherChars(File inputFile) throws FileNotFoundException
+	{
+		Scanner in = new Scanner(inputFile);
+		
+		int count = 0;
+		
+		while(in.hasNextLine())
+		{
+			String line = in.nextLine();
+			
+			for (int i = 0; i < line.length(); i++)
+			{
+				char letter = line.charAt(i);
+				boolean upperCase = Character.isUpperCase(letter);
+				boolean lowerCase = Character.isLowerCase(letter);
+				boolean whiteSpace = Character.isWhitespace(letter);
+				
+				if (!upperCase && !lowerCase && !whiteSpace)
 				{
 					count++;
 				}
