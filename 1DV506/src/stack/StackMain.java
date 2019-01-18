09be5baf42e624ea2f82e7d51873f75e7b124d6b
qexcc <mc222sn@student.lnu.se>
2019-01-18 11:37:10 +0100

    Adding interface and class

---
+++ 1DV506/src/stack/StackMain.java
@@ -0,0 +1,47 @@
+package stack;
+
+import java.util.Iterator;
+
+public class StackMain implements Stack
+{
+	public static void main(String[] args)
+	{
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void push(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<Object> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
