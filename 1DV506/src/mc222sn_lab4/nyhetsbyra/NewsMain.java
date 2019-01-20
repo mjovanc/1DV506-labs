54b147bd78fed9c2c77180ad25db396bd4a75c00
qexcc <mc222sn@student.lnu.se>
2019-01-20 22:44:51 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/nyhetsbyra/NewsMain.java
+++ 1DV506/src/mc222sn_lab4/nyhetsbyra/NewsMain.java
@@ -27,75 +27,44 @@ public class NewsMain
 			File newsSource = new File(path);
 			Scanner in = new Scanner(newsSource);
 			
+			NewsAgency reuters = new NewsAgency("Reuters");
+			
 			while (in.hasNextLine())
 			{
-				String[] line = in.nextLine().split(":");		
-				for (String l : line)
+				String[] line = in.nextLine().split(":");
+				String title = "";
+				String date = "";
+				String np = "";
+				
+				for (int i = 0; i < line.length; i++)
 				{
-					System.out.println(l);
+					switch(i)
+					{
+						case 0:
+							title = line[i];
+							break;
+						case 1:
+							date = line[i];
+							break;
+						case 2:
+							np = line[i];
+							break;
+					}
 				}
-				// System.out.println(newspaper);
+				
+				Newspaper newspaper = new Newspaper(np);
+				News n = new News(date, title, newspaper);
+				
+				newspaper.addNews(n);
+				reuters.registerNewspaper(newspaper);
 			}
-			
-			// Initializing Newspaper objects.
-			Newspaper aftonbladet = new Newspaper("Aftonbladet");
-			Newspaper expressen = new Newspaper("Expressen");
-			Newspaper smalandsposten = new Newspaper("Smålandsposten");
-			
-			// Initializing News objects
-			News news1 = new News(
-					"Sveriges mest jagade brottslingar är gripna",
-					"2019-01-20",
-					aftonbladet
-			);
-			News news2 = new News(
-					"Badou Jacks hälsning efter otäcka skadan",
-					"2019-01-20",
-					aftonbladet
-			);
-			News news3 = new News(
-					"Världens äldsta man död – blev 113 år",
-					"2019-01-20",
-					smalandsposten
-			);
-			
-			News news4 = new News(
-					"Kvinna slog till butikspersonal i ansiktet",
-					"2019-01-20",
-					smalandsposten
-			);
-			
-			// Adding news to corresponding newspaper.
-			aftonbladet.addNews(news1);
-			aftonbladet.addNews(news2);
-			smalandsposten.addNews(news3);
-			smalandsposten.addNews(news4);
-			
-			// Initializing NewsAgency object and register the newspapers to it.
-			NewsAgency reuters = new NewsAgency("Reuters");
-			reuters.registerNewspaper(aftonbladet);
-			reuters.registerNewspaper(expressen);
-			reuters.registerNewspaper(smalandsposten);
-			
-			// Sending news to all newspapers except the newspaper source.
+
 			reuters.sendNews();
 			
-			System.out.println("\nAftonbladet: ");
-			for (News n : aftonbladet.getAllNews())
+			for (News n : reuters.getAllNews())
 			{
 				System.out.println(n.displayNews());
-			}
-			
-			System.out.println("\nExpressen: ");
-			for (News n : expressen.getAllNews())
-			{
-				System.out.println(n.displayNews());
-			}
-			
-			System.out.println("\nSmålandsposten: ");
-			for (News n : smalandsposten.getAllNews())
-			{
-				System.out.println(n.displayNews());
+				
 			}
 		}
 		catch (FileNotFoundException e)
