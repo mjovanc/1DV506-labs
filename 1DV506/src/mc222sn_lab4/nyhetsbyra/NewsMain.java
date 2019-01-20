843039159960cc38987caa45d659d6a1f2dc9c73
qexcc <mc222sn@student.lnu.se>
2019-01-20 23:17:36 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/nyhetsbyra/NewsMain.java
+++ 1DV506/src/mc222sn_lab4/nyhetsbyra/NewsMain.java
@@ -1,9 +1,5 @@
 package mc222sn_lab4.nyhetsbyra;
 
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-
 /**
  * The NewsMain class.
  *
@@ -18,77 +14,67 @@ public class NewsMain
 	 *
 	 * @param args Takes an array of strings as parameter.
 	 */
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args)
 	{	
-		try
+		// Initializing Newspaper objects.
+		Newspaper aftonbladet = new Newspaper("Aftonbladet");
+		Newspaper expressen = new Newspaper("Expressen");
+		Newspaper smalandsposten = new Newspaper("SmÃ¥landsposten");
+		
+		// Initializing News objects
+		News news1 = new News(
+				"Sveriges mest jagade brottslingar är gripna",
+				"2019-01-20",
+				aftonbladet
+		);
+		News news2 = new News(
+				"Badou Jacks hälsning efter otäcka skadan",
+				"2019-01-20",
+				aftonbladet
+		);
+		News news3 = new News(
+				"Världens äldsta man död - blev 113 år",
+				"2019-01-20",
+				smalandsposten
+		);
+		
+		News news4 = new News(
+				"Kvinna slog till butikspersonal i ansiktet",
+				"2019-01-20",
+				smalandsposten
+		);
+		
+		// Adding news to corresponding newspaper.
+		aftonbladet.addNews(news1);
+		aftonbladet.addNews(news2);
+		smalandsposten.addNews(news3);
+		smalandsposten.addNews(news4);
+		
+		// Initializing NewsAgency object and register the newspapers to it.
+		NewsAgency reuters = new NewsAgency("Reuters");
+		reuters.registerNewspaper(aftonbladet);
+		reuters.registerNewspaper(expressen);
+		reuters.registerNewspaper(smalandsposten);
+		
+		// Sending news to all newspapers except the newspaper source.
+		reuters.sendNews();
+		
+		System.out.println("\nAftonbladet: ");
+		for (News n : aftonbladet.getAllNews())
 		{
-			// Getting the news from external file. Test textfile is included in zip-file.
-			String path = args[0];
-			File newsSource = new File(path);
-			Scanner in = new Scanner(newsSource);
-			
-			NewsAgency reuters = new NewsAgency("Reuters");
-			
-			while (in.hasNextLine())
-			{
-				String[] line = in.nextLine().split(":");
-				String title = "";
-				String date = "";
-				String np = "";
-				
-				for (int i = 0; i < line.length; i++)
-				{
-					switch(i)
-					{
-						case 0:
-							title = line[i];
-							break;
-						case 1:
-							date = line[i];
-							break;
-						case 2:
-							np = line[i];
-							break;
-					}
-				}
-				
-				Newspaper newspaper = new Newspaper(np);
-				News n = new News(date, title, newspaper);
-				
-				newspaper.addNews(n);
-				reuters.registerNewspaper(newspaper);
-			}
-
-			reuters.sendNews();
-			
-			for (Newspaper np : reuters.getRegisterdNewspapers())
-			{
-				System.out.println("\n" + np.getName());
-				for (News n : np.getAllNews())
-				{
-					boolean aftonbladet = n.getNews().getNewspaper().getName().equals("Aftonbladet");
-					boolean expressen = n.getNews().getNewspaper().getName().equals("Expressen");
-					boolean smalandsposten = n.getNews().getNewspaper().getName().equals("Smålandsposten");
-					
-					if (aftonbladet)
-					{
-						System.out.println(n.getNews().displayNews());
-					}
-					else if (expressen)
-					{
-						System.out.println(n.getNews().displayNews());
-					}
-					else if (smalandsposten)
-					{
-						System.out.println(n.getNews().displayNews());
-					}
-				}
-			}
-			
+			System.out.println(n.displayNews());
 		}
-		catch (FileNotFoundException e)
+		
+		System.out.println("\nExpressen: ");
+		for (News n : expressen.getAllNews())
 		{
-			System.err.println("No such file or directory!");
+			System.out.println(n.displayNews());
+		}
+		
+		System.out.println("\nSmÃ¥landsposten: ");
+		for (News n : smalandsposten.getAllNews())
+		{
+			System.out.println(n.displayNews());
 		}
 	}
 }
