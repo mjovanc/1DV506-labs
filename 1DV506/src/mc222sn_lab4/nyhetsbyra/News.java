64e4fe1488316fc154eeba15ca12b5b283721422
qexcc <mc222sn@student.lnu.se>
2019-01-20 20:49:18 +0100

    Adding code

--- 1DV506/src/mc222sn_lab4/nyhetsbyra/News.java
+++ 1DV506/src/mc222sn_lab4/nyhetsbyra/News.java
@@ -1,11 +1,25 @@
 package mc222sn_lab4.nyhetsbyra;
 
+/**
+ * The News class.
+ *
+ * @version 1.0
+ * @author Marcus Cvjeticanin
+ */
 public class News
 {
 	private String title;
 	private String date;
 	private Newspaper newspaper;
 	
+	/**
+	* Constructor for News.
+	* Setting news title, date and a newspaper object.
+	* 
+	* @param t A string with the title of the news.
+	* @param d A string with a date format YYYY-MM-DD
+	* @param np Newspaper object.
+	*/
 	public News(String t, String d, Newspaper np)
 	{
 		title = t;
@@ -13,16 +27,31 @@ public class News
 		newspaper = np;
 	}
 	
+	/**
+	 * Get the News object.
+	 * 
+	 * @return News object.
+	 */
 	public News getNews()
 	{
 		return this;
 	}
 	
+	/**
+	 * Get the Newspaper object of News object.
+	 * 
+	 * @return Newspaper object.
+	 */
 	public Newspaper getNewspaper()
 	{
 		return newspaper;
 	}
 	
+	/**
+	 * Get the news in a nice string display format.
+	 * 
+	 * @return String A string to display the news.
+	 */
 	public String displayNews()
 	{
 		return date + " - " + title + "\t\t// " + newspaper.getName();
